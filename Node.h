6485c4b288bb2f38b7db1ce989da1ac6bfd60ca5
ray <togneriraony@gmail.com>
2025-03-10 20:14:11 -0300

    caso 1

--- Node.h
+++ Node.h
@@ -1,6 +1,18 @@
 #ifndef _NODE_H_
 #define _NODE_H_
 
+#include <stdbool.h>
+
 typedef struct Node Node;
+
+Node *criaNode();
+
+void liberaNode(Node *node);
+
+int getNumElementosNode(Node *node);
+
+void* getChavesNode(Node *node);
+
+bool ehFolhaNode(Node *node);
 
 #endif
