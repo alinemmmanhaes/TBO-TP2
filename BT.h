56ef9a64ee26147abdfb79d5954c08c0fb4aa782
Maluu26 <142928665+Maluu26@users.noreply.github.com>
2025-03-19 21:36:22 -0300

    Update BT.h

--- BT.h
+++ BT.h
@@ -10,17 +10,19 @@ typedef struct Node Node;
 
 BT *criaBT(int ordem);
 
-void insereBT(BT *bt, int chave, int registro);
+void insereBT(BT *bt, int chave, int registro, FILE *arq);
 
-void removeBT(BT *bt, int chave);
+void removeBT(BT *bt, int chave, FILE *arq);
 
-Node *buscaBT(Node *node, Node *pai, int chave, int MODO_BUSCA);
+Node *buscaBT(Node *node, Node *pai, int chave, int MODO_BUSCA, FILE *arq);
 
 void printBT(BT* bt, FILE* arq);
 
 void insereChaveRegistro(Node *n, int chave, int registro, int ind);
 
 Node *getRaizBT(BT* bt);
+
+Node *diskRead(int offset, int ordem, FILE *fp);
 
 int getOrdemBT(BT *bt);
 
